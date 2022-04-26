6a5ee4bb5bdb1d328afa3672bfb5c4359c04ca74
goldman2g5 <tima.radchenko.84@mail.ru>
2022-04-26 10:13:41 +0300

    Initial commit

---
+++ prac10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.Tracing;
+using System.Reflection.Metadata;
+using System.Threading.Channels;
+using System.Transactions;
+
+namespace ConsoleApp5;
+
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string word = "huesos";
+        string wordUnknown = new (word.Select(_ => '?').ToArray());
+        int lives = 3;
+        while (wordUnknown.Contains('?') & lives > 0)
+        {
+            Console.Clear();
+            Console.WriteLine($"Lives: {lives}\n{wordUnknown}\nInput char > ");
+            char input = Console.ReadLine().ToCharArray()[0];
+                if (word.Contains(input))
+                {
+                    wordUnknown = new string(wordUnknown.Select((x, ind) => word[ind] == input ? input : x).ToArray());
+                    continue;
+                }
+
+                lives -= 1;
+        }
+        Console.WriteLine($"{word}\n{(lives > 0 ? "You won" : "You lost")}");
+    }
+}
