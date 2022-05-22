96c5b713007a88fc2b6aa12c84566f33dea4e7aa
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2022-05-22 07:05:11 +0300

    c

---
+++ prac10/Operation.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics.SymbolStore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace prac9;
+
+public class Operation
+{
+    public string Trigger;
+    public Func<string, string> Fn;
+    public static List<Operation> List = new();
+
+    public Operation(string trigger, Func<string, string> fn)
+    {
+        Trigger = trigger;
+        Fn = fn;
+        List.Add(this);
+    }
+
+    public static void CreateOperations()
+    {
+
+        string ToLower(string str)
+        {
+            return str.ToLower();
+        }
+
+        string ToUpper(string str)
+        {
+            return str.ToUpper();
+        }
+
+        string Replace(string var)
+        {
+            Console.WriteLine("Replace > ");
+            string oldValue = Console.ReadLine();
+            Console.WriteLine("With > ");
+            string newValue = Console.ReadLine();
+            return var.Replace(oldValue, newValue);
+        }
+
+        new Operation("tolower", ToLower);
+        new Operation("toupper", ToUpper);
+        new Operation("replace", Replace);
+    }
+}
--- prac10/Program.cs
+++ prac10/Program.cs
@@ -1,32 +1,154 @@
-﻿using System.Diagnostics.Tracing;
-using System.Reflection.Metadata;
-using System.Threading.Channels;
-using System.Transactions;
+﻿using System.Collections.Immutable;
+using System.Globalization;
+using System.IO.Enumeration;
+using Microsoft.VisualBasic.CompilerServices;
+using prac9;
 
-namespace ConsoleApp5;
-
-using System.Collections.Generic;
-
-class Program
+public class Program
 {
-    static void Main(string[] args)
+
+    private static void Main(string[] args)
     {
-        string word = "huesos";
-        string wordUnknown = new (word.Select(_ => '?').ToArray());
-        int lives = 3;
-        while (wordUnknown.Contains('?') & lives > 0)
+        Operation.CreateOperations();
+        
+        void RewriteLine(string caret, List<char> buffer)
         {
-            Console.Clear();
-            Console.WriteLine($"Lives: {lives}\n{wordUnknown}\nInput char > ");
-            char input = Console.ReadLine().ToCharArray()[0];
-                if (word.Contains(input))
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth - 1));
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(caret);
+            Console.Write(buffer.ToArray());
+        }
+
+        void Func(string value = "")
+        {
+            Console.WriteLine("(Leave blank for default) Input file name >");
+            string filename = Console.ReadLine();
+            string path =
+                @$"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\\{(filename == String.Empty ? DateTime.Now.ToLongDateString() : filename)}.txt";
+            if (!File.Exists(path))
+            {
+                File.CreateText(path).Close();
+            }
+
+            value = File.ReadLines(path).ToList()[0];
+            List<string> text = File.ReadLines(path).ToList();
+
+            while (true)
+            {
+                Console.Clear();
+                foreach (var i in text)
                 {
-                    wordUnknown = new string(wordUnknown.Select((x, ind) => word[ind] == input ? input : x).ToArray());
-                    continue;
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine($"{value}\nInput command >");
+                string input = Console.ReadLine().ToLower();
+
+                switch (input)
+                {
+                    case "save":
+                        break;
+                    
+                    case "edit":
+                        Console.Clear();
+                        string caret = "> ";
+                        string defaultValue = value;
+
+                        Console.WriteLine();
+
+                        List<char> buffer = defaultValue.ToCharArray().Take(Console.WindowWidth - caret.Length - 1)
+                            .ToList();
+                        List<List<char>> textbuffer = text.Select(x => x.ToCharArray().Take(Console.WindowWidth - caret.Length - 1)
+                            .ToList()).ToList();
+                        Console.Write(caret);
+                        Console.Write(buffer.ToArray());
+                        Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop);
+
+                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                        while (keyInfo.Key != ConsoleKey.Enter)
+                        {
+                            switch (keyInfo.Key)
+                            {
+                                case ConsoleKey.LeftArrow:
+                                    Console.SetCursorPosition(Math.Max(Console.CursorLeft - 1, caret.Length),
+                                        Console.CursorTop);
+                                    break;
+                                case ConsoleKey.RightArrow:
+                                    Console.SetCursorPosition(
+                                        Math.Min(Console.CursorLeft + 1, caret.Length + buffer.Count),
+                                        Console.CursorTop);
+                                    break;
+                                case ConsoleKey.UpArrow:
+                                    Console.SetCursorPosition(
+                                        Math.Min(Console.CursorLeft, caret.Length + buffer.Count),
+                                        Console.CursorTop);
+                                    buffer = textbuffer[textbuffer.IndexOf(buffer) + 1];
+                                    break;
+                                case ConsoleKey.Home:
+                                    Console.SetCursorPosition(caret.Length, Console.CursorTop);
+                                    break;
+                                case ConsoleKey.End:
+                                    Console.SetCursorPosition(caret.Length + buffer.Count, Console.CursorTop);
+                                    break;
+                                case ConsoleKey.Backspace:
+                                    if (Console.CursorLeft <= caret.Length)
+                                    {
+                                        break;
+                                    }
+
+                                    var cursorColumnAfterBackspace = Math.Max(Console.CursorLeft - 1, caret.Length);
+                                    buffer.RemoveAt(Console.CursorLeft - caret.Length - 1);
+                                    RewriteLine(caret, buffer);
+                                    Console.SetCursorPosition(cursorColumnAfterBackspace, Console.CursorTop);
+                                    break;
+                                case ConsoleKey.Delete:
+                                    if (Console.CursorLeft >= caret.Length + buffer.Count)
+                                    {
+                                        break;
+                                    }
+
+                                    var cursorColumnAfterDelete = Console.CursorLeft;
+                                    buffer.RemoveAt(Console.CursorLeft - caret.Length);
+                                    RewriteLine(caret, buffer);
+                                    Console.SetCursorPosition(cursorColumnAfterDelete, Console.CursorTop);
+                                    break;
+                                default:
+                                    var character = keyInfo.KeyChar;
+                                    if (character < 32)
+                                        break;
+                                    var cursorAfterNewChar = Console.CursorLeft + 1;
+                                    if (cursorAfterNewChar > Console.WindowWidth ||
+                                        caret.Length + buffer.Count >= Console.WindowWidth - 1)
+                                    {
+                                        break;
+                                    }
+
+                                    buffer.Insert(Console.CursorLeft - caret.Length, character);
+                                    RewriteLine(caret, buffer);
+                                    Console.SetCursorPosition(cursorAfterNewChar, Console.CursorTop);
+                                    break;
+                            }
+
+                            keyInfo = Console.ReadKey(true);
+                        }
+
+                        Console.Write(Environment.NewLine);
+
+                        value = new string(buffer.ToArray());
+                        continue;
+
+                    default:
+                        Console.Clear();
+                        value = Operation.List.Any(x => x.Trigger == input) ? Operation.List.Find(x => x.Trigger == input).Fn(value) : value;
+                        continue;
                 }
 
-                lives -= 1;
+                break;
+            }
+
+            File.WriteAllText(path, value);
         }
-        Console.WriteLine($"{word}\n{(lives > 0 ? "You won" : "You lost")}");
+
+        Func();
     }
 }
